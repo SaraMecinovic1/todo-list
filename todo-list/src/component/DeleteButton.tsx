fcf1f078f51d3e1122385d644d3ecf5c7bdb33d6
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-16 13:02:51 +0200

    map list

---
+++ todo-list/src/component/DeleteButton.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteButton = () => {
+  return <button>❌</button>;
+};
+
+export default DeleteButton;
--- todo-list/src/component/List.tsx
+++ todo-list/src/component/List.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import DeleteButton from "./DeleteButton";
 
+const initialTodos = ["buy bananas", "write letter", "walk ice"];
 const List = () => {
-  return <ul></ul>;
+  return (
+    <ul>
+      {initialTodos.map((todo, index) => (
+        <li
+          key={index}
+          className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%]"
+        >
+          <span className="line-through text-[#ccc]">{todo}</span> <DeleteButton />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default List;
