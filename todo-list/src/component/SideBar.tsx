54ffea92dc3179354cfcc9558c8cf18cfae6242f
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-16 11:51:17 +0200

    this version is valid

--- todo-list/src/component/SideBar.tsx
+++ todo-list/src/component/SideBar.tsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 
 const SideBar = () => {
   return (
-    <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.10] px-[25px] pt-[18px] pb-[28px] ">
+    <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.10] px-[25px] pt-[18px] pb-[28px] flex flex-col h-full">
       <AddTodoForm />
 
       <div className="mt-auto space-y-2">
