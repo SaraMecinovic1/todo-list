3813e5fadfe47191df93f01ac060ef179d430f98
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-16 11:50:33 +0200

    add login ang register buttons

---
+++ todo-list/src/component/AddTodoForm.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+
+const AddTodoForm = () => {
+  return (
+    <>
+      <h2 className="font-medium text-[#231d15 ]">Add a todo</h2>
+      <input
+        className="h-[45px] px-[16px] border border-black/[12%] rounded-[5px] my-[9px] text-[14px] block w-full"
+        type="text"
+      />
+      <Button />
+    </>
+  );
+};
+
+export default AddTodoForm;
---
+++ todo-list/src/component/Button.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button
+      className="h-[45px]  bg-[#473a2b] w-full text-white hover:bg-[#322618] rounded-[5px] cursor-pointer"
+      type="submit"
+    >
+      Add to list
+    </button>
+  );
+};
+
+export default Button;
