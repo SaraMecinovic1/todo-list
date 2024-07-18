c10cf7c8d3f6ecf9fa22fc35ca6e0a52c60372bf
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-18 14:49:59 +0200

    fix

--- todo-list/src/component/AddTodoForm.tsx
+++ todo-list/src/component/AddTodoForm.tsx
@@ -1,10 +1,10 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import Button from "./Button";
-import { TodosContext } from "../contexts/TodosContextProvider";
+import { useTodosContext } from "../lib/hooks";
 
 export default function AddTodoForm() {
   const [todoContent, setTodoContent] = useState("");
-  const { handleAddTodo } = useContext(TodosContext);
+  const { handleAddTodo } = useTodosContext();
 
   return (
     <form
