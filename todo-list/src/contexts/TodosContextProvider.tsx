97a5f5bc01bb50b0a2fefa3519f9f8f18e8d8f2a
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-19 23:23:37 +0200

    context edit

--- todo-list/src/contexts/TodosContextProvider.tsx
+++ todo-list/src/contexts/TodosContextProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { Todo } from "../lib/types";
 
 export const TodosContext = createContext<TTodosContext | null>(null);
