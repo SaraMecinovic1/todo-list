b238e8f8e7d07c1076646d9012935a3796c1690b
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-20 20:13:09 +0200

    making auth

--- todo-list/src/component/Button.tsx
+++ todo-list/src/component/Button.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 
 type ButtonProps = {
+  onClick: () => Promise<void>;
   buttonType?: "primary" | "secondary";
   children: React.ReactNode;
 };
 
-const Button = ({ buttonType, children }: ButtonProps) => {
+const Button = ({ onClick, buttonType, children }: ButtonProps) => {
   // isto kada bi pisali props.children/props.buttonType
   return (
     <button
+      onClick={onClick}
       className={`h-[45px]  bg-[#473a2b] w-full text-white
      hover:bg-[#322618] rounded-[5px] cursor-pointer ${
        buttonType === "secondary" ? "opacity-[85%]" : " "
--- todo-list/src/component/SideBar.tsx
+++ todo-list/src/component/SideBar.tsx
@@ -1,14 +1,27 @@
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import AddTodoForm from "./AddTodoForm";
 import Button from "./Button";
 
 const SideBar = () => {
+  const { login, register, isAuthenticated, user } = useKindeAuth();
+
   return (
     <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.10] px-[25px] pt-[18px] pb-[28px] flex flex-col h-full">
-      <AddTodoForm  />
+      <AddTodoForm />
 
       <div className="mt-auto space-y-2">
-        <Button buttonType="secondary">Login</Button>
-        <Button buttonType="secondary">Register</Button>
+        {isAuthenticated ? (
+          <p>Logged in as {user?.email}</p>
+        ) : (
+          <>
+            <Button onClick={login} buttonType="secondary">
+              Login
+            </Button>
+            <Button onClick={register} buttonType="secondary">
+              Register
+            </Button>
+          </>
+        )}
       </div>
     </section>
   );
