5c87087dcb4cab1cbadcc1127addeb244e1e6df2
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-07-20 19:43:59 +0200

    implemented kinde

--- todo-list/src/main.tsx
+++ todo-list/src/main.tsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./component/App.tsx";
 import "./index.css";
 import TodosContextProvider from "./contexts/TodosContextProvider.tsx";
-
+import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <TodosContextProvider>
-      <App />
-    </TodosContextProvider>
+    <KindeProvider
+      clientId="e819444596aa41c6a812ac66bbcec195"
+      domain="https://todolist1.kinde.com"
+      redirectUri="http://localhost:5174"
+      logoutUri="http://localhost:5174"
+    >
+      <TodosContextProvider>
+        <App />
+      </TodosContextProvider>
+    </KindeProvider>
   </React.StrictMode>
 );
